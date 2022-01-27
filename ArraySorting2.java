e2eafd2ae417d7bd842ca581666487af6230595e
Abhishek singh rajawat <94819827+abhi9971@users.noreply.github.com>
2022-01-27 20:26:39 +0530

    User Input to Solve Sorting Problems

--- ArraySorting2.java
+++ ArraySorting2.java
@@ -1,21 +1,37 @@
+import java.util.Scanner;
+
 public class ArraySorting2 {
     public static void main(String[] args) {
+        int n;
 
-        int a[]={10,2,5,1,9,3};
+        Scanner sc=new Scanner(System.in);
+
+        System.out.print("Enter the number of elements you want to store: ");
+
+        n=sc.nextInt();                                          //reading the number of elements from the that we want to enter
+
+        int[] array = new int[n];                                //creates an array in the memory of length n
+
+        System.out.println("Enter the elements of the array: ");
+
+        for(int i=0; i<n; i++)
+        {
+            array[i]=sc.nextInt();                                //reading array elements from the user
+        }
 
         int min_index=0;
 
         boolean flag=false;
 
-        for(int i=min_index; i<a.length;){
+        for(int i=min_index; i<array.length;){
 
-            int  min_number=a[i],b=min_number;
+            int  min_number=array[i],b=min_number;
 
-            for(int j=i+1;j<a.length;j++){
+            for(int j=i+1;j<array.length;j++){
 
-                if(a[j]<min_number){
+                if(array[j]<min_number){
 
-                    min_number=a[j];
+                    min_number=array[j];
 
                     b=min_number;
 
@@ -24,14 +40,14 @@ public class ArraySorting2 {
                     flag=true;
                 }
             }
-            if(flag) {
+            if(flag) {                                             //update the value of i for first loop
                 i = min_index + 1;
                 flag=false;
             }
             else{
                 i=i+1;
             }
-            System.out.print( " "+b );
+            System.out.print( " "+b );                            //print minimum value
         }
     }
 }
